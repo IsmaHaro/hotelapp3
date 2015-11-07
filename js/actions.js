f9d3054dc01289a8fb016160072bb1e4b5a47103
Isma <ismaharo>
2015-11-07 15:17:05 -0600

    cambios

--- js/actions.js
+++ js/actions.js
@@ -68,40 +68,6 @@ alert("Reserva: desconectado");
 	}
 };
 
-
-var almacen = {
-	db:null,
-	th:null,
-	np:null,
-	nh:null,
-	nd:null,
-	guardarReserva: function(th, np, nh, nd){
-alert("Creando database");
-		almacen.db = window.openDatabase("hotelApp", "1.0", "Hotel App", 200000);
-		almacen.th = th;
-		almacen.np = np;
-		almacen.nh = nh;
-		almacen.nd = nd;
-alert("aaaaaaaaaaaaaaa");
-		almacen.db.transaction(almacen.tablaReserva, almacen.error, almacen.exito);
-	},
-	error: function(e){
-		alert("Error, codigo: "+e.code);
-	},
-	exito: function(){
-		alert("Reserva guardada en dispositivo, en espera de sincronizacion");
-		//$.mobile.loading("hide");
-	},
-	tablaReserva: function(tx){
-alert("Guardando reserva");
-		tx.executeSql("CREATE TABLE IF NOT EXISTS reservas(th, np, nh, nd)");
-		tx.executeSql("INSERT INTO reservas(th, np, nh, nd) 
-			VALUES ("+almacen.th+","+almacen.np+","+almacen.nh+","+almacen.nd+")");
-alert("Reserva guardada en BD");
-	}
-};
-
-
 $(fn.ready);
 
 //fn.init();
